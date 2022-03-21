dcceec7ef7b8747d2867fcd7f2427523b3730700
Birkeholm <mikkel.birkeholm@gmail.com>
2022-03-21 16:33:38 +0100

    Colors working in localstorage
    Mostly. Some weird things happens. Also spaghetti code.

--- index.js
+++ index.js
@@ -1,53 +1,59 @@
-let fontName = ''
 let listEl = document.querySelectorAll('.list-el')
 const colorList = document.getElementById('color-list')
 let colorBoxes = ''
 let savedColors = document.querySelectorAll('.colorbox')
 const addColorBtn = document.getElementById('add-color')
-const testBtn = document.getElementById('test-btn')
-const clearBtn = document.getElementById('clear-btn')
-const storage = chrome.storage.sync
+let myColors = []
+let myNotes = []
+let colorsFromLocalStorage = JSON.parse( localStorage.getItem("myColors") )
+let notesFromLocalStorage = JSON.parse( localStorage.getItem("myNotes") )
+let notePad = document.getElementById('notepad')
+let mainView = document.getElementById('main-view')
+let outerWrap = document.getElementById('outer-wrapper')
+let contentWrap = document.getElementById('content-wrapper')
+let noteWrapper = document.getElementById('note-wrapper')
+
+let savedNote = document.getElementById('saved-note')
+let noteInput = document.getElementById('note-input')
+let saveInput = document.getElementById('save-note')
 
 
-const fontTemplate = `
-  <li class="saved-font hover-effect">
-    <span>${fontName}</span><img src="duplicate-icon.svg" alt="" width="25" class="copy-icon">
-  </li>
-`
-function liTemplate(bgColor) { 
-  return `
-  <li class="list-el">
-    <div id="delete-btn" class="delete-btn"></div>
-    <div class="colorbox hover-effect" style="background-color:${bgColor}" value="${bgColor}">
-    </div>
-  </li>
-`
+
+notePadTemplate = `
+string text string text
+`;
+
+colorPads = `
+<div id="color-wrapper">
+<h3>Brand Colors</h3>
+<span id="add-color" class="hover-effect"></span>
+<ul id="color-list">
+</ul>
+</div>
+`;
+
+
+
+if (colorsFromLocalStorage) {
+  myColors = colorsFromLocalStorage
+  renderColors()
 }
 
-
-/* --- This is just some chrome storage testing :) ---- */
-
-function saveColor(id, value) {
-  this.id = id;
-  this.value = value;
-}
-
-// storage.set({color: '#fff'}, function() {
-// });
-
-testBtn.addEventListener('click', function() {
-  storage.set({"savedColors": saveColor(hund, blue)}, function(){
-  })
+saveInput.addEventListener("click", function() {
+  savedNote.innerText = noteInput.value
+  myNotes = savedNote.innerText
+  noteInput.value = ""
 })
 
-clearBtn.addEventListener('click', function(){
-storage.clear()
+mainView.addEventListener("click", function() {
+  noteWrapper.classList.add("hidden");
+  contentWrap.classList.remove("hidden");
 })
 
-
-
-/* --- This is just some chrome storage testing end ---- */
-
+notePad.addEventListener("click", function() {
+  contentWrap.classList.add("hidden");
+  noteWrapper.classList.remove("hidden");
+})
 
 /* --- CONTEXT MENU, don't mind this yet ----  */
 
@@ -55,47 +61,82 @@ document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
 });
 
-
  /* --- CONTEXT MENU END----  */
 
 
 addColorBtn.addEventListener("click", function() {
-  let colorList = document.getElementById('color-list')
+  body = document.body;
+  outerWrap = document.getElementById('outer-wrapper')
   const eyeDropper = new EyeDropper()
-
+  body.classList.add("hidden");
+  outerWrap.classList.add("hidden");
   eyeDropper.open().then(result => {
+    body.classList.remove("hidden");
+    outerWrap.classList.remove("hidden");
     colorValue = result.sRGBHex
-    colorList.insertAdjacentHTML('beforeend', liTemplate(colorValue));
+    myColors.push(colorValue)
+    localStorage.setItem("myColors", JSON.stringify(myColors) )
+    renderColors()
+    addAni()
+  }) .catch(e => {
+    body.classList.remove("hidden");
+    outerWrap.classList.remove("hidden");
+    return
+  })
+})
 
-    let currentColor = document.getElementsByClassName('colorbox')
-    addDelete()
 
-    for (let i = 0; i < currentColor.length; i++) {
+
+function renderColors() {
+  colorsFromLocalStorage = JSON.parse( localStorage.getItem("myColors") );
+  myColors = colorsFromLocalStorage;
+  listOfColors = "";
+  for (let i = 0; i < myColors.length; i++) {
+    listOfColors += `
+      <li class="list-el">
+        <div id="delete-btn" class="delete-btn"></div>
+        <div class="colorbox hover-effect" style="background-color:${myColors[i]}" value="${myColors[i]}" title=${myColors[i]}>
+        </div>
+      </li>
+  `
+  }
+  colorList.innerHTML = listOfColors
+  addCopy()
+  addDelete()
+}
+
+function addCopy() {
+  let currentColor = document.getElementsByClassName('colorbox')
+  for (let i = 0; i < currentColor.length; i++) {
       currentColor[i].id = 'idColor' + [i+1];
       currentColor[i].addEventListener("click", () => {
           let elementCopy = currentColor[i].getAttribute('value')
           navigator.clipboard.writeText(elementCopy);
          })
     }
-  })
-    .catch(e => {
-      alert('Something went wrong or the action was cancelled')
-    })
-
-})
+}
 
 
 function addDelete() {
   let deleteBtn = document.getElementsByClassName('delete-btn')
-    for (let i = 0; i < deleteBtn.length; i++) {
+  for (let i = deleteBtn.length - 1; i >= 0; i--) {
       deleteBtn[i].addEventListener("click", function() {
         this.parentNode.remove();
+        removeColor(i)
+        renderColors()
       })
 }}
 
-
-function addFont() {
-    let fontList = document.getElementById('font-list')
-    fontList.innerHTML = fontList.innerHTML + fontTemplate
+function removeColor(e) {
+    myColors.splice(e, 1);
+    localStorage.setItem("myColors", JSON.stringify(myColors) )
 }
 
+
+function addAni() {
+  let listEl = document.querySelectorAll('.list-el')
+  for (let i = 0; i < listEl.length; i++) {
+    lastItem = listEl.length - 1;
+    listEl[lastItem].classList.add('spawnAni')
+  }
+}
